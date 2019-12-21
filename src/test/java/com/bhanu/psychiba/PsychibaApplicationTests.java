55de14b870dbe70bba6f6da0c24c8deab174eacc
unknown <bhanur05@gmail.com>
2019-12-21 19:23:06 +0530

    Initial commit

---
+++ src/test/java/com/bhanu/psychiba/PsychibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhanu.psychiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
